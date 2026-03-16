0fe7108cdd0840cbfd3338c06ccd0e57ca46c09d
w3villa-nitin-rajput <nitin.rajput@w3villa.com>
2026-03-16 16:31:15 +0530

    test cron job

--- app/jobs/expire_plans_job.rb
+++ app/jobs/expire_plans_job.rb
@@ -3,6 +3,7 @@ class ExpirePlansJob < ApplicationJob
 
   def perform
     # Find users whose plans have expired
+    Rails.logger.info "CRON CHECK: ExpirePlansJob is running at #{Time.now}"
     expired_users = User.where("plan != ? AND plan_expires_at <= ?", User.plans[:free], Time.current)
     
     expired_users.find_each do |user|
