cecac35c01f340682f4fd5391042f31755be78f7
w3villa-nitin-rajput <nitin.rajput@w3villa.com>
2026-03-17 17:07:26 +0530

    change port number

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
-  port:                 2525,
+  port:                 587,
   domain:               'gmail.com',
   user_name:            ENV['EMAIL_USER'],
   password:             ENV['EMAIL_PASS'], # 16-character App Password
