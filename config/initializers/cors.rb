8dba955153b0fe62dea8a76ddb9130233f650c9d
w3villa-nitin-rajput <nitin.rajput@w3villa.com>
2026-03-17 12:17:05 +0530

    cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,20 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173', ENV.fetch("FRONTEND_URL") { "" }
+    # Use an array to handle multiple origins cleanly
+    allowed_origins = ['http://localhost:5173']
+    
+    # Add the production URL if the environment variable exists
+    if ENV["FRONTEND_URL"].present?
+      # We strip trailing slashes to ensure the match is exact
+      allowed_origins << ENV["FRONTEND_URL"].delete_suffix('/')
+    end
+
+    origins allowed_origins
 
     resource '*',
       headers: :any,
       expose: ['Content-Disposition'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true # Add this if you use sessions/cookies/Devise
   end
 end
